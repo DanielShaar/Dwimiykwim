719cf53359dd43c153d81be3da79e38c417aa65b
Ziv Scully <zivscully@gmail.com>
2015-04-25 21:55:30 -0400

    Implement bipartite matching.

---
+++ bipartite-matching.scm
@@ -0,0 +1,64 @@
+(define (perfect-matching xs-to-ys ys-to-xs xs-exposed ys-exposed)
+  (if (or (null? xs-exposed) (null? ys-exposed))
+      ;; TODO: extract matching from this.
+      ys-to-xs
+      (let* ((x (car xs-exposed))
+             (new-xs (cdr xs-exposed))
+             ;; Augmenting path from y to x for some exposed y.
+             (path (augmenting-path xs-to-ys ys-to-xs x ys-exposed)))
+        (if (null? path)
+            ;; TODO: return matching so far?
+            #f
+            (let* ((new-edges (flip xs-to-ys ys-to-xs path))
+                   (new-xs-to-ys (car new-edges))
+                   (new-ys-to-xs (cadr new-edges))
+                   (y (car path))
+                   (new-ys (remove (partial-apply eq? y) ys-exposed)))
+              (perfect-matching new-xs-to-ys new-ys-to-xs new-xs new-ys))))))
+
+(define (augmenting-path xs-to-ys ys-to-xs x ys-exposed)
+  (define (search-x old-path x)
+    (if (memq x old-path)
+        #f
+        (let ((new-path (cons x old-path))
+              (neighbors (cdr (assq x xs-to-ys))))
+          (any (partial-apply search-y new-path) neighbors))))
+  (define (search-y old-path y)
+    (if (memq y old-path)
+        #f
+        (let ((new-path (cons y old-path)))
+          (if (memq y ys-exposed)
+              new-path
+              (let ((neighbors (cdr (assq y ys-to-xs))))
+                (any (partial-apply search-x new-path) neighbors))))))
+  (search-x '() x))
+
+(define (flip old-xs-to-ys old-ys-to-xs old-path)
+  (if (or (null? old-path) (null? (cdr old-path)))
+      ;; These are flipped because in the last iteration, the path is (x),
+      ;; which means old-xs-to-ys is actually ys-to-xs....
+      (list old-ys-to-xs old-xs-to-ys)
+      (let* ((y (car old-path))
+             (x (cadr old-path))
+             (new-path (cdr old-path))
+             (new-xs-to-ys (remove-edge old-xs-to-ys x y))
+             (new-ys-to-xs (add-edge old-ys-to-xs y x)))
+        (flip new-ys-to-xs new-xs-to-ys new-path))))
+
+(define (add-edge xs-to-ys x y)
+  (let* ((x-neighbors (assq x xs-to-ys)))
+    (if x-neighbors
+        (cons (cons* x y (cdr x-neighbors))
+              (del-assq x xs-to-ys))
+        (cons (list x y) xs-to-ys))))
+
+(define (remove-edge xs-to-ys x y)
+  (let* ((x-neighbors (assq x xs-to-ys)))
+    (if x-neighbors
+        (cons (cons x (remove (partial-apply eq? y) (cdr x-neighbors)))
+              (del-assq x xs-to-ys))
+        xs-to-ys)))
+
+;; Victory!
+;; (perfect-matching '((x1 y1 y2) (x2 y1 y2 y3) (x3 y1)) '() '(x1 x2 x3) '(y1 y2 y3))
+;; ;Value 32: ((y1 x3) (y3 x2) (y2 x1))
--- load.scm
+++ load.scm
@@ -7,11 +7,12 @@
 (define generic-evaluation-environment
   (access generic-evaluation-environment user-initial-environment))
 
-(load "library" user-initial-environment)
 (load "utils" user-initial-environment)
+(load "bipartite-matching" user-initial-environment)
 (load "ghelper" user-initial-environment)
 (load "syntax" user-initial-environment)
 (load "rtdata" user-initial-environment)
+(load "library" user-initial-environment)
 
 (load "analyze" generic-evaluation-environment)
 (load "repl" generic-evaluation-environment)
