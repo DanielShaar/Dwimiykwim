0359a094458064fc705a4e3a8ecd80f160cd23a0
Ziv Scully <zivscully@gmail.com>
2015-05-02 15:18:30 -0400

    Fix typo in tests.scm.

--- tests.scm
+++ tests.scm
@@ -11,7 +11,7 @@
 (flip-all '((x1 y1 y2) (x2 y1 y2 y3) (x3 y1)))
 ;Value: ((y1 x3 x2 x1) (y3 x2) (y2 x2 x1))
 
-(perfect-matching
+(semiperfect-matching
  '((x1 y1 y2) (x2 y1 y2 y3) (x3 y1))
  '()
  '(x1 x2 x3)
